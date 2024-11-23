594fdba5f4ee51064449bbfcc9a950ddeb6e5064
zwh <codebyte@126.com>
2024-11-24 05:28:05 +0800

    feat(C/Code001): 添加日志模块并更新相关代码
    - 新增 log 模块，包括 log.c、log.h 和 makefile 文件
    - 修改项目 makefile，集成 log 模块的编译和链接
    - 在 sub.c 中添加日志记录，演示日志模块的使用

---
+++ C/Code001/lib/log/log.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "log.h"
+
+void log_info(char *str)
+{
+    printf("-----INFO:----%s\n", str);
+}
--- C/Code001/src/sub.c
+++ C/Code001/src/sub.c
@@ -1,10 +1,13 @@
 #include "stdio.h"
 #include "test.h"
 #include "sub.h"
+#include "log.h"
 
 void printSub()
 {
+    log_info("sub-start-log");
     printf("sub-begin\n");
     printTest();
     printf("sub-end\n");
+    log_info("sub-end-log");
 }
